5383f18da194eb73608b84645c5e164fdfb32fba
Jesus Leon Rangel <jesusrangel.255@gmail.com>
2023-06-06 19:47:56 -0400

    Init project

---
+++ src/main/java/com/jarl/trading/admin/jarvis/bot/JarvisAdminApplication.java
@@ -0,0 +1,13 @@
+package com.jarl.trading.admin.jarvis.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarvisAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarvisAdminApplication.class, args);
+	}
+
+}
